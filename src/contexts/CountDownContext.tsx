b7007db91795639ae740861e4bd16f30a5dc289a
Mateus Guerreiro <mateusguerreiro11@hotmail.com>
2021-03-02 11:36:26 -0300

    tentativa_5.2

--- src/contexts/CountDownContext.tsx
+++ src/contexts/CountDownContext.tsx
@@ -22,11 +22,10 @@ export function CountDownProvider({ children }: CountDownProviderProps) {
 
     const { startNewChallenge } = useContext(ChallengesContext)
 
-<<<<<<< HEAD
-    const totalMinutes = (20 * 60)
-=======
+
     const totalMinutes = (25 * 60)
->>>>>>> parent of 43db042 (alterando_tempo-tentativa-2)
+
+
 
     const [time, setTime] = useState(totalMinutes)
 
