94116e79a6b4b7696daa44c61967487833ac384a
Mateus Guerreiro <mateusguerreiro11@hotmail.com>
2021-03-02 10:12:09 -0300

    diminuindo_tempo_countdown-para-testes

--- src/contexts/CountDownContext.tsx
+++ src/contexts/CountDownContext.tsx
@@ -22,7 +22,7 @@ export function CountDownProvider({ children }: CountDownProviderProps) {
 
     const { startNewChallenge } = useContext(ChallengesContext)
 
-    const totalMinutes = (25 * 60)
+    const totalMinutes = (0.05 * 60)
 
     const [time, setTime] = useState(totalMinutes)
 
