17f7e464220d9a85b9ded6e4af9db5b851a851cd
Mateus Guerreiro <mateusguerreiro11@hotmail.com>
2021-03-13 15:56:37 -0300

    alteracaonaapi3

--- src/pages/api/api.ts
+++ src/pages/api/api.ts
@@ -1,7 +1,7 @@
-import dotenv from 'dotenv';
+//import dotenv from 'dotenv';
 import axios from 'axios';
 
-dotenv.config();
+//dotenv.config();
 
 const api = axios.create({
     baseURL: process.env.BASE_URL,
