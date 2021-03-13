76e6746feff53cbd4af30e2cd978b2626c2e1f37
Mateus Guerreiro <mateusguerreiro11@hotmail.com>
2021-03-13 17:56:30 -0300

    alteracaonaapi8

--- src/pages/api/api.ts
+++ src/pages/api/api.ts
@@ -5,4 +5,30 @@ const api = axios.create({
     baseURL: process.env.BASE_URL,
 });
 
+
+
+
+
+
+
 export default api;
+
+
+
+/*
+const apiSecret = process.env.BASE_URL
+
+async function api(request, response) {
+
+    const users = await fetch(`${apiSecret}`)
+
+    const usersResponse = await users.json()
+    const user = usersResponse
+
+    response.json({
+        users: user
+    })
+}
+
+export default api;
+*/
